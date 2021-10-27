a22740f59774e2d08e7e3c096a3758d13cc2bb32
renan-lemes <61756319+renan-lemes@users.noreply.github.com>
2021-10-27 11:53:28 -0300

    add Dash1.1.2

--- armário/dash_v1.1/Dash.py
+++ armário/dash_v1.1/Dash.py
@@ -1,7 +1,7 @@
 import builtins
 from logging import _STYLES 
-from math import acos 
 from os import path
+from time import sleep
 from numpy import select
 import streamlit as st
 import investpy as ip
@@ -10,7 +10,7 @@ import plotly.graph_objs as go
 import plotly.express as px
 from streamlit.legacy_caching.caching import cache
 from streamlit.proto.PlotlyChart_pb2 import Figure
-from web_scrapping import iee_b3, import_ibov
+from web_scrapping import iee_b3, import_ibov, import_ind
 
 
 countries = ['brazil', 'united states']  
@@ -22,8 +22,8 @@ end_date = datetime.today()                      # end sendo a data de hoje #
 #   exportando os dados    #
 
 #  @cache para poder puxar os dados apenas uma vez # 
-#   função que puxa os dados da B3#
-#@st.cache 
+#   função que puxa os dados da B3    #
+@st.cache 
 def iE():
     iee = iee_b3()  
     return iee
@@ -44,7 +44,7 @@ indicadores = ['DY', 'PL', 'PEG_RATIO', 'P_PV', 'EV_EBITIDA', 'EV_EBIT', 'P_EBIT
                'CAGR_RECEITAS_5_ANOS', 'CAGR_LUCROS_5_ANOS']
 
 # Função grafico de pizza #
-#@st.cache
+@st.cache
 def sun_b(var):
 
     fig_2 = go.Figure(go.Sunburst(
@@ -60,6 +60,12 @@ def sun_b(var):
         height=600,)
 
     return fig_2
+# Grafico teste #
+def Graftest(df):
+    fig = [go.Scatter(
+        x = df.index,
+        y = df['Close'])]
+    return fig
 
 # função de formatação de data  # 
 #@st.cache
@@ -88,25 +94,95 @@ def plotCandleStick(df, acao='ticket'):
 # Definindo o  dash 2#
 
 # Função que define novo dash  # 
-def Dash_2():
-    st.markdown(' ## Novo Dash 2  - Tabelas ')
-    col1, col2, col3 = st.columns(3)
-    col1.markdown(' ## Tabela 1')
-    col2.markdown(' ## Tabela 2')
-    col3.markdown(' ## Tabela 3')
+def Dash_2(iee):
+    st.sidebar.title('Menu 📰 De Notícias  ') # 
+    st.sidebar.markdown('---')
+    st.title('  Dash 2 ')
+    st.markdown('---')
+    
+    #stock_select_side = st.sidebar.selectbox("Selecione o ativo:" )
+    
+    col1,col2,= st.columns(2)
+    container2 = st.container()
+    col_side1, col_side2, col_side3 = st.sidebar.columns(3)
+
+    
+    container3 = st.container()
+    container4 = st.container()
+    
+    #df1, df2 = import_ind(stock_select_side)
+    with col1:
+        from_date = st.date_input('De:', start_date)  
+    with col2:
+        to_date = st.date_input('Para:', end_date)
+    
+    #stock_selec = st.sidebar.selectbox('Escolha a ação')
+    #colunas dos valores #
+    with container3:
+        with col_side1: # Coluna sidebar 1#
+            st.metric(label="Price", value=df2.loc[0][1], delta='',delta_color="inverse")
+            st.metric("Wind", "9", "-8%")
+            st.metric("Wind", "9", "-8%")
+            st.metric(label="Price", value=4, delta=-0.5,delta_color="inverse")
+            
+
+        with col_side2: # Coluna sidebar 2 #
+            st.metric(label="Vale3", value='12%', delta=123,delta_color="off")
+            st.metric("Wind", "15", "+8%")
+            st.metric("Wind", "9", "-8%")
+            st.metric(label="Price", value=4, delta=-0.5,delta_color="inverse")
+            
+        with col_side3: # Coluna sidebar 3#
+            st.metric(label="Wind", value=9, delta='-16%', delta_color="off")
+            st.metric("Wind", "17", '8%')
+            st.metric(label="Price", value=4, delta=-0.5,delta_color="inverse")
+    
+            
+    #with container4:
+    #    with col_side4:
+    #        st.metric("Wind", "9", "-8%")
+    #    with col_side5:
+    #        st.metric("Wind", "15", "+8%")
+    #    with col_side6:
+    #        st.metric("Wind", "17", '8%')
+    
+    with container2:
+        st.markdown('---')
+        stock_select = st.selectbox("Selecione o ativo:", [i for i in iee.Empresa])
+        df1,df2 = import_ind(iee[iee.Empresa == stock_select].loc[0][1])
+        #interval_select = st.selectbox("Selecione o timeframe:", intervals)
+        #grafico#
+    
+    
+
+    if from_date > to_date:
+        st.error('Data de ínicio maior do que data final')
+    else:
+        df = consultar_acao(iee[iee.Empresa == stock_select].Acao.values[0], 'brazil', format_date(
+            from_date), format_date(to_date), 'Daily')
+        try : 
+            fig = Graftest(df)
+            grafico_candle = st.plotly_chart(fig, use_container_width=True) 
+            st.markdown('---')
+        except Exception as e:
+            st.error(e)  
+    st.dataframe(df)
+
+    
+
+
+    
 
 # Definindo o Dash 3  #
 
 # Função que define novo dash  # 
 def dash_3():
+    
     st.markdown(' ## Novo Dash 3 - Gráficos')
     c = st.container()
     c.markdown(' # Grafico 1')
     c.markdown(' # Grafico 2')
     c.markdown(' # Grafico 3')
-
-
-
 
 carregar_dados = 0
 iee = iE() #carregadno dados  da função iE # 
@@ -115,11 +191,11 @@ dash = ['Dash 1', 'Dash 2', 'Dash 3']   # variavel dash recebe uma lista de dash
 newDash = st.sidebar.selectbox('Selecione o  Dash', dash) # newDash recebe um seletor de caixa  #
 formatacao1   = st.sidebar.markdown("---")
 # if que é usado para fazer um novo dash    #
-if newDash == 'Dash 2':
-    Dash_2() # Função Dash 2#
+if newDash == 'Dash 2' :
+    Dash_2(iee) # Função Dash 2#
 elif newDash == 'Dash 3':
     dash_3()    # Função Dash 3
-#Caso contrario motrar dash 1#    
+#Caso contrario motra o dash 1#    
 else:
     barra_lateral = st.sidebar.title('Menu 🌫️   ')
     barra_lateral = st.sidebar.empty()
@@ -127,14 +203,14 @@ else:
     
     stock_select = st.selectbox("Selecione o ativo:", [i for i in iee.Empresa]) # stock select recebe um seletor de ativo  # 
     formatacao13   = st.markdown("---")
-    from_date = st.sidebar.date_input('De:', start_date)  # #
+    from_date = st.sidebar.date_input('De:', start_date)  
     to_date = st.sidebar.date_input('Para:', end_date)
     interval_select = st.sidebar.selectbox("Selecione o timeframe:", intervals)
     formatacao2 = st.sidebar.markdown("---")
     grafico_line = st.empty()   #Grafico de linha  recebendo vazio   #
     grafico_candle = st.empty() #Grafico de candle recebendo vazio   #
-    grafico_pizza = st.empty()  #gRAFICO DE pizza recebendo vazio    #
-    if from_date > to_date: # Se from data foir maior que to date#
+    grafico_pizza = st.empty()  #grafico DE pizza recebendo vazio    #
+    if from_date > to_date: # Se from data for maior que to date#
         st.sidebar.error('Data de ínicio maior do que data final') 
     else:
         df = consultar_acao(iee[iee.Empresa == stock_select].Acao.values[0], 'brazil', format_date(
@@ -145,7 +221,7 @@ else:
             
             grafico_pizza = st.plotly_chart(figpizza, use_container_width=True) # Grafico de pizza usando a variavel figpizza em que o dataframe foi transformado em figura#
 
-            grafico_line = st.line_chart(df.Close)  #pegando apenas os dados de df.Close para usar no grafico de linhas#
+            #grafico_line = st.line_chart(df.Close)  #pegando apenas os dados de df.Close para usar no grafico de linhas#
 
             container = st.container() 
             with container:
